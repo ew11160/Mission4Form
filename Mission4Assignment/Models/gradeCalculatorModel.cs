a0b2e43df68181b978ef423fe2831c21111f60fd
ew11160 <ea11160@byu.edu>
2023-02-06 14:31:58 -0700

    First run of mission 5

---
+++ Mission4Assignment/Models/gradeCalculatorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4Assignment.Models
+{
+    public class gradeCalculatorModel
+    {
+        [Required] //// validation
+        public string assignments { get; set; }
+        [Required]
+        public string groupProject { get; set; }
+        [Required]
+        public string quizzes { get; set; }
+        [Required]
+        public string midterm { get; set; }
+        [Required]
+        public string final { get; set; }
+        [Required]
+        public string intex { get; set; }
+    }
+}
